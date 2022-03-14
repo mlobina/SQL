f07b4ad9a1f5119d79ff298536f7162b783191d5
Marina Lobina <mvlobina@gmsil.com>
2022-03-14 17:00:16 +0300

    add the seventh part

---
+++ SQL_part_seventh_mysql/02.2_insert_customers.sql
@@ -0,0 +1,12 @@
+INSERT INTO customers(
+    first_name,
+    last_name,
+    email
+)
+VALUES(
+    'Sveta',
+    'Star',
+    'sveta@test.com'
+);
+
+-- SAVEPOINT save_1;
---
+++ SQL_part_seventh_postgres/02.3_insert_orders.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO orders(
+   amount_billed,
+   customer_id 
+)
+VALUES(
+    103.12,
+    6
+);
